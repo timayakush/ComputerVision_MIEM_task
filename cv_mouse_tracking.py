29756cff9ff311fd00ed40390e1f5106e47998a3
timayakush <113637539+timayakush@users.noreply.github.com>
2023-01-26 18:27:42 +0300

    Add files via upload

--- cv_mouse_tracking.py
+++ cv_mouse_tracking.py
@@ -6,10 +6,15 @@ width, height = autopy.screen.size()
 hands = mp.solutions.hands.Hands(static_image_mode=False, max_num_hands=1, min_tracking_confidence=0.5,
                                  min_detection_confidence=0.5)
 mpDraw = mp.solutions.drawing_utils
+f1, f2 = False, False
 while True:
     _, img = cap.read()
+    img = cv2.flip(img, 1)
     result = hands.process(img)
-    print(result)
+    if not f1 and f2:
+        print('\nHand appeared')
+    if f1 and not f2:
+        print('\nHand disappeared')
     if result.multi_hand_landmarks:
         for id, lm in enumerate(result.multi_hand_landmarks[0].landmark):
             h, w, _ = img.shape
@@ -17,7 +22,15 @@ while True:
             cv2.circle(img, (cx, cy), 3, (255, 0, 255))
             if id == 8:
                 cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
-                autopy.mouse.move(cx * width / w, cy * height / h)
+                try:
+                    autopy.mouse.move(cx * width / w, cy * height / h)
+                    print(cx, cy, sep=' ', end='; ')
+                    f1 = f2
+                    f2 = True
+                except ValueError:
+                    f1 = f2
+                    f2 = False
+                    continue
         mpDraw.draw_landmarks(img, result.multi_hand_landmarks[0], mp.solutions.hands.HAND_CONNECTIONS)
     cv2.imshow("Hand tracking", img)
     cv2.waitKey(1)
