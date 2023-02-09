50564190a267b493b5148d9ad4c6590a64a16792
timayakush <113637539+timayakush@users.noreply.github.com>
2023-02-09 12:53:42 +0300

    1.2 version

--- cv_mouse_tracking.py
+++ cv_mouse_tracking.py
@@ -11,26 +11,35 @@ while True:
     _, img = cap.read()
     img = cv2.flip(img, 1)
     result = hands.process(img)
-    if not f1 and f2:
-        print('\nHand appeared')
     if f1 and not f2:
         print('\nHand disappeared')
     if result.multi_hand_landmarks:
         for id, lm in enumerate(result.multi_hand_landmarks[0].landmark):
             h, w, _ = img.shape
+            f1 = f2
+            f2 = True
+            if not f1 and f2:
+                print('Hand appeared')
             cx, cy = int(lm.x * w), int(lm.y * h)
             cv2.circle(img, (cx, cy), 3, (255, 0, 255))
+            if id == 4:
+                cx_2, cy_2 = cx, cy
             if id == 8:
                 cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+                cx_1, cy_1 = cx, cy
                 try:
                     autopy.mouse.move(cx * width / w, cy * height / h)
                     print(cx, cy, sep=' ', end='; ')
-                    f1 = f2
-                    f2 = True
                 except ValueError:
-                    f1 = f2
-                    f2 = False
                     continue
+                if ((cx_1 - cx_2) ** 2 + (cy_1 - cy_2) ** 2) ** 0.5 < 50:
+                    try:
+                        autopy.mouse.click()
+                    except ValueError:
+                        continue
         mpDraw.draw_landmarks(img, result.multi_hand_landmarks[0], mp.solutions.hands.HAND_CONNECTIONS)
+    else:
+        f1 = f2
+        f2 = False
     cv2.imshow("Hand tracking", img)
     cv2.waitKey(1)
